58500e96ff5b017e1655a4e81657c35dfac58ba9
Pema Lama <pema.srp72@gmail.com>
2021-11-16 11:54:29 -0500

    added UserFilter

--- src/MasterList.js
+++ src/MasterList.js
@@ -2,10 +2,12 @@ import SearchBar from "./SearchBar"
 import {useEffect,useState} from "react"
 import ListItem from "./ListItem"
 import AddNewForm from "./AddNewForm";
+import UserFilter from "./UserFilter";
 
 function MasterList(){
     const [lists,setList] = useState([]);
     const [searchedFood, setSearchedFood] = useState("")
+    const [selectedUser,setSelectedUser] = useState("")
     
 
     useEffect(() => {
@@ -13,13 +15,18 @@ function MasterList(){
         .then(res => res.json())
         .then(data => setList(data))
     },[])
-    
+
     const visibleList = lists.filter((list) => (list.rname.toLowerCase().includes(searchedFood.toLowerCase())) || (list.favdish.toLowerCase().includes(searchedFood.toLowerCase())))
+    const userList = lists.filter((list) => (list.user === selectedUser))
+    const newFilter = selectedUser ? userList : visibleList
+
 
     return(
         <div>
+        <h1> Buckets Of Foods List </h1>
         <SearchBar setSearchedFood={setSearchedFood} search={searchedFood}/>
-        {visibleList.map(list => <ListItem key={list.id} id={list.id} user={list.user} rname={list.rname} address={list.address} favDish={list.favdish} price={list.price} image={list.image}/>)}
+        <UserFilter lists={lists} setSelectedUser={setSelectedUser}/>
+        {newFilter.map(list => <ListItem key={list.id} id={list.id} user={list.user} rname={list.rname} address={list.address} favDish={list.favdish} price={list.price} image={list.image}/>)}
         <AddNewForm />
         </div>
     )
