b1dddf185e501ab39297ae1ef170b94a35b00583
Max <max.edison@yahoo.com>
2021-11-16 13:53:57 -0500

    pausing my updates to grab updated AddNewForm

--- src/UserFilter.js
+++ src/UserFilter.js
@@ -1,9 +1,15 @@
+import {useState} from "react";
+
 function UserFilter({lists,setSelectedUser}){
-    function handleClick(e){
+
+   const [buttonsList, setButtonsList] = useState([])
+
+   function handleClick(e){
        setSelectedUser(e.target.innerText)
     }
     const userList = lists.map((list) => <button onClick={handleClick}>{list.user}</button>)
- return( 
+
+    return( 
 
     userList
  )
